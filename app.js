f89a491436258f4af0991570513a67bfa2e2c380
user <akashlakhera.me@gmail.com>
2023-07-05 19:34:15 +0530

    sec

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express=require("express")
 const {home,posts}=require("./controllers/home")
 app=express()
+const port=process.env.PORT||4000
 app.use(express.json())
 app.get("/",home)
 app.get("/posts",posts)
-app.listen(4000,()=>{
+app.listen(port,()=>{
     console.log("server is running")
 })
